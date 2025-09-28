a28bb41721a1af2e3873500b2e41ead490aeb522
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-09-29 01:07:37 +0500

    Task2

---
+++ Tyuiu.ChetvertnyhAI.Sprint1.Task2.V20.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint1.Task2.V20.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint1.Task2.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            int y = 3;
+            var res = ds.CalculateSquaresSumm(x, y);
+            Assert.AreEqual(13, res);
+        }
+    }
+}
