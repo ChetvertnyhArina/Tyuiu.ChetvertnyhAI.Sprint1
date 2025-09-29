4c7911b21a5d5835eb0398675f67100ce81c686b
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-09-30 02:18:57 +0500

    Task7

--- Tyuiu.ChetvertnyhAI.Sprint1.Task7.V26.Test/DataServiceTest.cs
+++ Tyuiu.ChetvertnyhAI.Sprint1.Task7.V26.Test/DataServiceTest.cs
@@ -8,10 +8,10 @@ namespace Tyuiu.ChetvertnyhAI.Sprint1.Task7.V26.Test
         public void ValidExpression()
         {
             DataService ds = new DataService();
-            double x = 2;
-            double y = 4;
-            double wait = 0.7539026588331511;
-            var res = ds.Calculate(x, y);
+            double x = 5;
+            double y = 5;
+            double wait = 0.591;
+            var res = Math.Round(ds.Calculate(x, y), 3);
             Assert.AreEqual(wait, res);
 
         }
