1eee8cba45f3c8e8eb40e1149635eed7af38e29c
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-09-29 23:03:53 +0500

    Task5

---
+++ Tyuiu.ChetvertnyhAI.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint1.Task5.V1.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double x1 = 4;
+            double y1 = 3;
+            double x2 = 8;
+            double y2 = 6;
+
+            DataService ds = new DataService();
+            double res = ds.DistanceBetweenDots(x1, y1, x2, y2);
+
+            int result = Convert.ToInt32(res);
+            int wait = 5;
+            Assert.AreEqual(wait, result);
+
+
+        }
+    }
+}
---
+++ Tyuiu.ChetvertnyhAI.Sprint1.Task5.V1/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint1.Task5.V1.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнила: Четвертных А. И. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #1                                                              *");
+        Console.WriteLine("* Выполнила: Четвертных Арина Игоревна | ИСТНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая решает следующую задачу:                    *");
+        Console.WriteLine("* Найти расстояние между двумя точками с заданными координатами (x, y).   *");
+        Console.WriteLine("* Ответ привести к целому с помощью класса Convert.                       *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите X1:");
+        double x1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите Y1:");
+        double y1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите X2:");
+        double x2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите Y2:");
+        double y2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        
+        int res = Convert.ToInt32(ds.DistanceBetweenDots(x1, y1, x2, y2));
+        Console.WriteLine(res);
+        Console.ReadKey();
+
+    }
+}
