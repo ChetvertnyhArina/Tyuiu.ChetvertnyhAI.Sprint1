62ee98ce7108d81a4b67215caa79f95f2b359ec9
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-09-30 01:46:44 +0500

    Task7

---
+++ Tyuiu.ChetvertnyhAI.Sprint1.Task7.V26.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint1.Task7.V26.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint1.Task7.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double wait = 1.1884054251388094;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.ChetvertnyhAI.Sprint1.Task7.V26/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint1.Task7.V26.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнила: Четвертных А. И. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #26                                                             *");
+        Console.WriteLine("* Выполнила: Четвертных Арина Игоревна | ИСТНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+        Console.WriteLine("* исходным значениям данных,вводимых пользователем. Ответ округлите до 3  *");
+        Console.WriteLine("* знаков после запятой.                                                 *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("      sin x^2 + y     x * y - 12       ");
+        Console.WriteLine("z =   -----------  -  ----------        ");
+        Console.WriteLine("         y + 1         34 + x^2      ");
+        double x, y;
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение Y:");
+        y = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(Math.Round(ds.Calculate(x, y), 3));
+        Console.ReadKey();
+
+    }
+}
