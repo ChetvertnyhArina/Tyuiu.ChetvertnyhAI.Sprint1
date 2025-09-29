b95085f7c96c819830795b6ef270b6d15a3375cf
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-09-29 23:13:28 +0500

    Task5

--- Tyuiu.ChetvertnyhAI.Sprint1.Task5.V1.Lib/DataService.cs
+++ Tyuiu.ChetvertnyhAI.Sprint1.Task5.V1.Lib/DataService.cs
@@ -1,11 +1,13 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint1;
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.ChetvertnyhAI.Sprint1.Task5.V1.Lib
 {
     public class DataService : ISprint1Task5V1
     {
         public int DistanceBetweenDots(double x1, double y1, double x2, double y2)
         {
-            double res = Math.Sqrt((Math.Pow(x2 - x1, 2)) + (Math.Pow(y2 - y1, 2)));
+            double res = Math.Sqrt((Math.Pow(x2 - x1, 2)) + (Math.Pow(y2 - y1, 2)))+0.5;
+            
             return (int)res;
         }
     }
